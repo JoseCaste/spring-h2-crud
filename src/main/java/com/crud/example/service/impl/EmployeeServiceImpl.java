772a2d0db676f7426d9361990a0a23d16a28b968
Jose Castellanos <jcast@softserveinc.com>
2023-03-17 11:55:04 -0600

    Added application properties for prod environment

--- src/main/java/com/crud/example/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/crud/example/service/impl/EmployeeServiceImpl.java
@@ -4,13 +4,23 @@ import com.crud.example.model.Employee;
 import com.crud.example.repository.EmployeeRepository;
 import com.crud.example.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Value("${devOps.path}")
+    private String devOpsPath;
+
+    @PostConstruct
+    private void getValueFromProperties(){
+        System.out.println(devOpsPath);
+    }
     @Override
     public Employee getAndEmployee() throws Exception {
         return employeeRepository.findById(1L).orElseThrow(Exception::new);
